696abb995b06ff14c97fbe1582e2902daae2a1e9
chrisyandev <czyanhome@hotmail.com>
2023-04-05 17:09:10 -0700

    connection works

--- NekoBot/Program.cs
+++ NekoBot/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using Microsoft.Extensions.Configuration;
+
+namespace NekoBot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddUserSecrets(typeof(Program).Assembly, true)
+                .Build();
+
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = config["TOKEN"],
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
